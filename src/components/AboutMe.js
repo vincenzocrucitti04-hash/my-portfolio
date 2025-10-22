30b31aecaa2e2fb075c8a2a01727342744ddc698
Vincenzo Crucitti <229733393+vincenzocrucitti04-hash@users.noreply.github.com>
2025-10-22 14:41:59 +0200

    fix: improve text clarity and flow in AboutMe and Hobbies sections

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,27 +8,26 @@ export default function AboutMe() {
       <h2 id="chi-sono-heading">Chi sono</h2>
       <article id="aboutme">
         <p>
-          Ciao! Sono Vincenzo, ho 21 anni e questo è il mio portfolio...
+          Ciao! Sono Vincenzo, ho 21 anni e questo è il mio portfolio!
           <br />
-          Non sto qui a raccontarti la storia di tutta la mia vita, ma ti
-          descrivo un po&apos; quali sono le mie passioni e ambizioni.
+          Tranquillo non sono qui per raccontarti la storia di tutta la mia
+          vita, ma ti descrivo un po' le mie passioni e ambizioni.
           <br />
-          Sono un appassionato di fotografia, la fotografia per me è più di uno
-          scatto: ma un modo per fermare attimi che altrimenti scivolerebbero
-          via. Attraverso l&apos;obiettivo riesco a dare spazio alla mia
-          curiosità e al desiderio di scoprire cose nuove.
+          Per iniziare sono un appassionato di fotografia: per me è una forma di
+          memoria, è un modo per fermare attimi, raccontare momenti, prima che
+          scivolino via. L'obiettivo è il modo più autentico con cui riesco a
+          dare spazio alla mia curiosità e al desiderio di scoprire cose nuove.
           <br />
           Sin da bambino, sognavo di diventare uno sviluppatore, affascinato
-          dall&apos;idea di creare qualcosa dal nulla che potesse essere utile
-          e, perché no, rendermi famoso.
+          dall'idea di creare qualcosa dal nulla che potesse essere utile e,
+          perché no, rendermi famoso.
           <br />
           Quel sogno è cresciuto con me. Oggi, sto studiando come Front-End
-          Developer. Ho già creato qualche progetto, ed in ogni progetto ho
-          cercato di dare uno stile unico che possa lasciare un segno in chi lo
-          guarda.
+          Developer e ho già creato qualche progetto; in ognuno di questi ho
+          cercato uno stile unico che possa lasciare un segno in chi lo guarda.
           <br />
           Sei pronto a scoprirli? Qui sotto ho lasciato dei link se vuoi dacci
-          un&apos;occhiata, e grazie per il tuo tempo!
+          un'occhiata, e grazie per il tuo tempo!
         </p>
       </article>
     </section>
